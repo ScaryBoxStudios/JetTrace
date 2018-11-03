593fda19af3727e8a5bdc9900383978cc7094798
TheCrafter <dimitrisvlh@gmail.com>
2018-11-03 17:23:04 +0200

    Implemented infinite background scrolling

--- scripts/main.gd
+++ scripts/main.gd
@@ -1,27 +1,23 @@
 extends Node
 
 export (PackedScene) var Jet
-# class member variables go here, for example:
-# var a = 2
-# var b = "textvar"
-var jet;
+
 var started = false;
 
 func _ready():
-	# Called when the node is added to the scene for the first time.
-	# Initialization here
-	var screensize = get_viewport().get_visible_rect().size
-
-	# Spawn jet
-	jet = Jet.instance()
-	jet.position = get_viewport().get_mouse_position()
-	add_child(jet)
+    # Called when the node is added to the scene for the first time.
+    # Initialization here
+    var screensize = get_viewport().get_visible_rect().size
 
 func _process(delta):
-	if Input.is_action_pressed("ui_cancel"):
+    $camera.position.y = $camera.position.y - 0.5
+    #jet.position.y = $camera.position.y
+    var mouse_pos = get_viewport().get_mouse_position()
+    $jet.move(mouse_pos.x, (mouse_pos.y - 512) + $camera.position.y)
+    if Input.is_action_pressed("ui_cancel"):
         get_tree().quit()
 
 func _input(event):
-	# Mouse in viewport coordinates
-	if event is InputEventMouseMotion:
-		jet.position = event.position
+    # Mouse in viewport coordinates
+    #if event is InputEventMouseMotion:
+        #jet.move(event.position.x, (event.position.y - 512) + $camera.position.y)
