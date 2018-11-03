1580cdd309665cf9ca3e5b82437a87f9ec3f3914
TheCrafter <dimitrisvlh@gmail.com>
2018-11-03 15:39:29 +0200

    Close app when Esc is pressed

--- scripts/main.gd
+++ scripts/main.gd
@@ -17,10 +17,9 @@ func _ready():
 	jet.position = get_viewport().get_mouse_position()
 	add_child(jet)
 
-#func _process(delta):
-#	# Called every frame. Delta is time since last frame.
-#	# Update game logic here.
-#	pass
+func _process(delta):
+	if Input.is_action_pressed("ui_cancel"):
+        get_tree().quit()
 
 func _input(event):
 	# Mouse in viewport coordinates
