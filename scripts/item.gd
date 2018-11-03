16c507d55dd319ed66dbdf5c2a305ac2076f6604
TheArtist <agorglouk@gmail.com>
2018-11-03 17:49:05 +0200

    Added physics components to items and self destruction

--- scripts/item.gd
+++ scripts/item.gd
@@ -1,15 +1,21 @@
 extends Node2D
 
+const size = 20.0
+
 func _ready():
-	# Called when the node is added to the scene for the first time.
-	# Initialization here
-	pass
+    # Called when the node is added to the scene for the first time.
+    # Initialization here
+    $collision.shape.radius = size
+    $visibility.connect("screen_exited", self, "_on_visibility_screen_exited")
 
 func _draw():
-	# Your draw commands here
-	draw_circle(Vector2(0.0, 0.0), 20, Color(1.0, 0.0, 0.0))
+    # Your draw commands here
+    draw_circle(Vector2(0.0, 0.0), size, Color(1.0, 0.0, 0.0))
+
+func _on_visibility_screen_exited():
+    queue_free()
 
 #func _process(delta):
-#	# Called every frame. Delta is time since last frame.
-#	# Update game logic here.
-#	pass
+#    # Called every frame. Delta is time since last frame.
+#    # Update game logic here.
+#    pass
