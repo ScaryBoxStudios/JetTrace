1bcec318b0e85b887e8d1d0f5a6eb24abb0bc72e
TheArtist <agorglouk@gmail.com>
2018-11-03 15:31:39 +0200

    Implemented basic jet movement

--- scripts/main.gd
+++ scripts/main.gd
@@ -4,16 +4,25 @@ export (PackedScene) var Jet
 # class member variables go here, for example:
 # var a = 2
 # var b = "textvar"
+var jet;
+var started = false;
 
 func _ready():
 	# Called when the node is added to the scene for the first time.
 	# Initialization here
-	var jet = Jet.instance()
-	add_child(jet)
 	var screensize = get_viewport().get_visible_rect().size
-	jet.position = screensize / 2.0
+
+	# Spawn jet
+	jet = Jet.instance()
+	jet.position = get_viewport().get_mouse_position()
+	add_child(jet)
 
 #func _process(delta):
 #	# Called every frame. Delta is time since last frame.
 #	# Update game logic here.
 #	pass
+
+func _input(event):
+	# Mouse in viewport coordinates
+	if event is InputEventMouseMotion:
+		jet.position = event.position
